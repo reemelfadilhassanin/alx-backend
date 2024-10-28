72efd8b38c7b418d01479a5af667818abd7dbe88
root <root@Reem.localdomain>
2024-10-28 06:01:55 +0300

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-Module to defines function to 
+Module to
 calculate the index range for pagination.
 """
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    This calculate the 
-    start and end index for pagination.
+    Calculate the start and end index for pagination.
 
     Args:
         page (int): The page number (1-indexed).
@@ -20,8 +20,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     if page < 1 or page_size < 1:
         raise ValueError("Both page and page_size must be greater than 0")
-    
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    
+
     return (start_index, end_index)
