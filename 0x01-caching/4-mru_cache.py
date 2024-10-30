0515d49e167829555df1e19cebb5ec033241068d
root <root@Reem.localdomain>
2024-10-30 17:06:23 +0300

    mru cashing

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,49 +1,39 @@
 #!/usr/bin/env python3
-"""MRU Caching
-    """
-
-
+"""Task 4: MRU Caching.
+"""
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """A class `MRUCache` that inherits from
-       `BaseCaching` and implements a caching system
-       that discards the most recently used item.
+    """A class `MRUCache` that inherits
+       from `BaseCaching` and is a caching system
     """
-
     def __init__(self):
-        """Initialize the cache
+        """Initializes the cache.
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Adds an item in the cache.
-
-        Args:
-            key: The key to store the item.
-            item: The item to store in the cache.
         """
         if key is None or item is None:
             return
-        # If the key is already in the cache, remove it first
-        if key in self.cache_data:
-            del self.cache_data[key]
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Remove the last inserted item (most recently used)
-            mru_key = next(reversed(self.cache_data))  # Get the last key
-            del self.cache_data[mru_key]
-            print("DISCARD:", mru_key)
-        # Add the new item
-        self.cache_data[key] = item
+        if key not in self.cache_data:
+            # If adding this item exceeds the max limit, discard the MRU item
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)  # Mark as recently used
+        else:
+            self.cache_data[key] = item  # Update existing item
+            self.cache_data.move_to_end(key)  # Mark as recently used
 
     def get(self, key):
         """Retrieves an item by key.
-
-        Args:
-            key: The key for the item to retrieve.
-
-        Returns:
-            The value associated with the key, or None if not found.
         """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)  # Mark as recently used
         return self.cache_data.get(key, None)
